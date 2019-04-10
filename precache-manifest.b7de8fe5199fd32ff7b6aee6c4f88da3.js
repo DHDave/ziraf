c973c64c98bf4137bcaa79c19eb6b05961a142ee
David Hajos <dh.it.email@gmail.com>
2019-04-09 22:24:16 -0300

    Updates

---
+++ precache-manifest.b7de8fe5199fd32ff7b6aee6c4f88da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ziraf/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "285afea318fa8aeeb175",
+    "url": "/ziraf/static/js/runtime~main.4aab6666.js"
+  },
+  {
+    "revision": "b52411b6f40e0460e91e",
+    "url": "/ziraf/static/js/main.636b1a26.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ziraf/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b52411b6f40e0460e91e",
+    "url": "/ziraf/static/css/main.0aea5941.chunk.css"
+  },
+  {
+    "revision": "dd8b0de39e85dba58f9ce82791217a59",
+    "url": "/ziraf/index.html"
+  }
+];
